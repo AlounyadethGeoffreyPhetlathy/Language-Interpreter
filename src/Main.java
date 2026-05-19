0bc9c78c16fc95c2bd8eda218f64f18704514ad2
AlounyadethGeoffreyPhetlathy <102637451+AlounyadethGeoffreyPhetlathy@users.noreply.github.com>
2026-05-19 12:06:32 -0500

    Some changes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
 	public static void main(String[] args) {
-		// Read file, line by line
+		// Create all necessary objects
+		MyFileHandler mfh = new MyFileHandler();
+		
+		// Setting up the text file
+		mfh.AssignFile("./Test.txt");
+		System.out.println(mfh.laoFile.exists());
+//		mfh.OpenFile(mfh.getFile());
+		
+		// Test writeline
+		System.out.println(mfh.laoFile);
 	}
 
 }
---
+++ src/MyFileHandler.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class MyFileHandler {
+	// fields will be package-protected for now
+	int line;
+	File laoFile;
+	Scanner scanobj;
+	
+	// Getters
+	public File getFile() {
+		return laoFile;
+	}
+	
+	// Important methods
+	public void AssignFile(String path) {
+		laoFile = new File(path);
+	}
+	
+	public void OpenFile(File laoFile) {
+		try {
+			scanobj = new Scanner(laoFile);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+}
